8ca57066a4a6cff3e423c6d91bf1af336233fd8c
benrau87 <benrau87@users.noreply.github.com>
2017-04-14 12:46:44 -0500

    Update startbrew.sh

--- startbrew.sh
+++ startbrew.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /etc/datasploit
+cd /etc/datasploit/core
 print_status "${YELLOW}Starting webserver${NC}"
-brew services restart mongodb
-brew services restart rabbitmq 
-C_FORCE_ROOT=root celery -A core worker -l info --concurrency 20  
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000  
