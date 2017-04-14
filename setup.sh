d7d9cd956f6e88d6a16f0cc32522bc65f7aa4707
benrau87 <benrau87@users.noreply.github.com>
2017-04-14 11:54:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,27 +75,25 @@ fi
 print_status "${YELLOW}Adding repos${NC}"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 &>> $logfile
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list &>> $logfile
-echo 'deb http://www.rabbitmq.com/debian/ testing main' |sudo tee /etc/apt/sources.list.d/rabbitmq.list &>> $logfile
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add - &>> $logfile
 
 # Add Debian Wheezy backports repository to obtain init-system-helpers
-gpg --keyserver pgpkeys.mit.edu --recv-key 7638D0442B90D010
-gpg -a --export 7638D0442B90D010 | sudo apt-key add -
-echo 'deb http://ftp.debian.org/debian wheezy-backports main' | sudo tee /etc/apt/sources.list.d/wheezy_backports.list
+gpg --keyserver pgpkeys.mit.edu --recv-key 7638D0442B90D010 &>> $logfile
+gpg -a --export 7638D0442B90D010 | sudo apt-key add - &>> $logfile
+echo 'deb http://ftp.debian.org/debian wheezy-backports main' | sudo tee /etc/apt/sources.list.d/wheezy_backports.list &>> $logfile
 
 # Add Erlang Solutions repository to obtain esl-erlang
-wget -O- https://packages.erlang-solutions.com/debian/erlang_solutions.asc | sudo apt-key add -
-echo 'deb https://packages.erlang-solutions.com/debian wheezy contrib' | sudo tee /etc/apt/sources.list.d/esl.list
+wget -O- https://packages.erlang-solutions.com/debian/erlang_solutions.asc | sudo apt-key add - &>> $logfile
+echo 'deb https://packages.erlang-solutions.com/debian wheezy contrib' | sudo tee /etc/apt/sources.list.d/esl.list &>> $logfile
 
-sudo apt-get update
-sudo apt-get install init-system-helpers socat esl-erlang -y
+sudo apt-get update &>> $logfile
+sudo apt-get install init-system-helpers socat esl-erlang -y &>> $logfile
 
 # continue with RabbitMQ installation as explained above
-wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add - &>> $logfile
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list &>> $logfile
 
-sudo apt-get update
-sudo apt-get install rabbitmq-server -y 
+sudo apt-get update &>> $logfile
+sudo apt-get install rabbitmq-server -y  &>> $logfile
 error_check 'Repos added'
 
 print_status "${YELLOW}Updating sources${NC}"
