e6cf327c1eed9f7ecc27d23ab952a98fcddfc982
benrau87 <benrau87@users.noreply.github.com>
2017-04-17 11:14:43 -0500

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -5,7 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
    
 apt-get update
-apt-get install docker-ce -y
+apt-get install -y docker-engine
   
 docker pull appsecco/datasploit
   
