e733cfea98982672b02f02c490a26b3fc8aefbf8
benrau87 <benrau87@users.noreply.github.com>
2017-04-25 16:48:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -107,9 +107,4 @@ cd datasploit
 pip install -r requirements.txt &>> $logfile
 #pip install django celery django-celery whois wad pymongo termcolor &>> $logfile
 mv config_sample.py config.py
-mkdir datasploitDb
-mongod --dbpath datasploitDb 
-#brew services restart mongodb 
-#brew services restart rabbitmq
-#C_FORCE_ROOT=root celery -A core worker -l info --concurrency 20       
-#python manage.py runserver 0.0.0.0:8000  &
+
